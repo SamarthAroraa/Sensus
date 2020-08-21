618331624a957eb426ef0415c4f39f69833ad9ec
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-08-21 18:24:28 +0530

    Fixed UTC time, now local time will be used.

--- models/entry.js
+++ models/entry.js
@@ -4,36 +4,33 @@ const mongoose = require("mongoose");
 //          Entry Schema            //
 //////////////////////////////////////
 
-const entrySchema = new mongoose.Schema(
-  {
+const entrySchema = new mongoose.Schema({
     user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
     },
     text: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     mood: {
-      type: String,
-      required: true,
-      default: "#fff",
+        type: String,
+        required: true,
+        default: "#ffffff",
     },
     createDate: {
-      type: Date,
-      required: true,
-      default: Date.now(),
+        type: Date,
+        required: true,
+        default: Date(Date.now() + Date.getTimezoneOffset()), //offset returns the milliseconds difference of the current location from the UTC timezone.
     },
     updateDate: {
-      type: Date,
-      default: Date.now(),
+        type: Date,
+        default: Date(Date.now() + Date.getTimezoneOffset()), //Adding the offset sets the date to the local one.
     },
-  },
-  {
+}, {
     timestamps: true,
-  }
-);
+});
 
 const Entry = mongoose.model("Entry", entrySchema);
 
