eb7c97e61c0e135b395f0c94563f32a818858cbb
pragatikumar <pragatikumar2000@gmail.com>
2020-08-22 01:03:40 +0530

    users will be granted access to their respective accounts now after being authenticated

--- models/user.js
+++ models/user.js
@@ -1,60 +1,87 @@
-const mongoose = require("mongoose");
-const multer = require("multer");
-const path = require("path");
-// const AVATAR_PATH = path.join("/uploads/users/avatars");
-//////////////////////////////////////
-//          User Schema             //
-//////////////////////////////////////
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    avatar: {
-      type: String,
-    },
-    
-  },
-  {
-    timestamps: true,
-  }
-);
+const mongoose= require('mongoose');
 
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "..", AVATAR_PATH));
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
+const userSchema= new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+}, {
+    timestamps: true
 });
 
-//static methods
-userSchema.statics.uploadedAvatar = multer({
-  storage: storage,
-  fileFilter: function (req, file, cb) {
-    console.log("check:", path.extname(file.originalname));
-    var ext = path.extname(file.originalname);
-    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+const User= mongoose.model('User', userSchema);
+
+module.exports= User;
+
+// const mongoose = require("mongoose");
+// const multer = require("multer");
+// const path = require("path");
+// const AVATAR_PATH = path.join("/uploads/users/avatars");
+// //////////////////////////////////////
+// //          User Schema             //
+// //////////////////////////////////////
+// const userSchema = new mongoose.Schema(
+//   {
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     avatar: {
+//       type: String,
+//     },
+    
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// let storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, "..", AVATAR_PATH));
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + "-" + Date.now());
+//   },
+// });
+
+// //static methods
+// userSchema.statics.uploadedAvatar = multer({
+//   storage: storage,
+//   fileFilter: function (req, file, cb) {
+//     console.log("check:", path.extname(file.originalname));
+//     var ext = path.extname(file.originalname);
+//     if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
       
-      return cb(new Error("Only images are allowed"));
-    }
-    cb(null, true);
-  },
-  limits: {
-    fileSize: 1024 * 1024,
-  },
-}).single("avatar");
-userSchema.statics.avatarPath = AVATAR_PATH;
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+//       return cb(new Error("Only images are allowed"));
+//     }
+//     cb(null, true);
+//   },
+//   limits: {
+//     fileSize: 1024 * 1024,
+//   },
+// }).single("avatar");
+// userSchema.statics.avatarPath = AVATAR_PATH;
+// const User = mongoose.model("User", userSchema);
+// module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const analzeController = require("../controllers/sentimentAPI");
 
-router.get("/", function (req, res) {
-  return res.render("./pages/test_home", {
-    layout: "_layout.ejs",
-    title: "Test Home",
-    sentiment: null,
-    final_score: null,
-    text: null,
-  });
-});
-router.post("/analyze", analzeController.analyze);
+const homeController= require('../controllers/home_controller');
+
+router.use('/users', require('./users'));
+
+router.get('/', homeController.home);
+
 module.exports = router;
