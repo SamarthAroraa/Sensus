946a4d879978e83a438f54d200f764e926556514
pragatikumar <pragatikumar2000@gmail.com>
2020-08-23 16:41:11 +0530

    The testing page is displayed after logging into your respective account so that the entry is not visible to everyone

---
+++ controllers/testing-controller.js
@@ -0,0 +1,9 @@
+module.exports.home = function (req, res) {
+    return res.render("profile", {
+      title: "Profile",
+      sentiment: null,
+      final_score: null,
+      text: null,
+      color: null,
+    });
+  };
