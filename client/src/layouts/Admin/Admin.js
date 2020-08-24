f46cf3b61381d8d30fc77a28e85c2cc7e2529372
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-08-24 15:20:07 +0530

    added logos in assets->img and changed the favicon and side nav icon

--- client/src/layouts/Admin/Admin.js
+++ client/src/layouts/Admin/Admin.js
@@ -28,7 +28,7 @@ import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
 import routes from "routes.js";
 
-import logo from "assets/img/react-logo.png";
+import logo from "assets/img/logo-green-fill.svg";
 
 var ps;
 
