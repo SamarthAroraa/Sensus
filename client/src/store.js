662566305a6b9ef3fe3b6fe66d21532237827c72
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-08-30 18:01:40 +0530

    reversed changes

--- client/src/store.js
+++ client/src/store.js
@@ -7,6 +7,9 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 export default store;
