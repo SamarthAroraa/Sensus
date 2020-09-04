64bad52d9c82e32353e44a46150f4292204ba847
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-09-04 19:16:05 +0530

    data api

--- controllers/api/v1/data.js
+++ controllers/api/v1/data.js
@@ -3,10 +3,10 @@ const User = require("../../../models/user");
 const SentimentApi = require("../../sentimentAPI");
 const ObjectId = require("mongodb").ObjectID;
 
-module.exports.getAnnualData =async  function (req, res) {
-  var complete={}
-  var global = {}
-  global.S= await Entry.aggregate(
+module.exports.getAnnualData = async function (req, res) {
+  var complete = {};
+  var global = {};
+  global.S = await Entry.aggregate(
     [
       // { $match: { user: ObjectId(req.body.user) } },
       { $match: { category: "S" } },
@@ -28,16 +28,16 @@ module.exports.getAnnualData =async  function (req, res) {
           dailyentries: { $push: { day: "$_id.day", count: "$count" } },
         },
       },
-      // {
-      //   $group: {
-      //   average: { "$avg": "$count" },
+      {
+        $group: {
+          // average: { $avg: "$count" },
 
-      //     _id: { year: "$_id.year" },
-      //     monthlyentries: {
-      //       $push: { month: "$_id.month", average: "$first" },
-      //     },
-      //   },
-      // },
+          _id: { year: "$_id.year" },
+          monthlyentries: {
+            $push: { month: "$_id.month", average: "$dailyentries" },
+          },
+        },
+      },
     ],
 
     (err, result) => {
@@ -48,9 +48,96 @@ module.exports.getAnnualData =async  function (req, res) {
       return result;
     }
   );
-  var data = { };
+  global.N = await Entry.aggregate(
+    [
+      { $match: { category: "N" } },
+
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+            day: { $dayOfMonth: "$createdAt" },
+          },
+          count: { $sum: 1 },
+        },
+      },
+
+      {
+        $group: {
+          _id: { year: "$_id.year", month: "$_id.month" },
+          dailyentries: { $push: { day: "$_id.day", count: "$count" } },
+        },
+      },
+
+      {
+        $group: {
+          _id: { year: "$_id.year" },
+          monthlyentries: {
+            $push: { month: "$_id.month", dailyentries: "$dailyentries" },
+          },
+        },
+      },
+    ],
+
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } // TODO handle error
+      // console.log(result);
+      return result;
+    }
+  );
+
+  //Pipline to accumulate all Happy [H] classified entries
+
+  global.H = await Entry.aggregate(
+    [
+      { $match: { category: "H" } },
+
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+            day: { $dayOfMonth: "$createdAt" },
+          },
+          count: { $sum: 1 },
+        },
+      },
+
+      {
+        $group: {
+          _id: { year: "$_id.year", month: "$_id.month" },
+          dailyentries: { $push: { day: "$_id.day", count: "$count" } },
+        },
+      },
+
+      {
+        $group: {
+          _id: { year: "$_id.year" },
+          monthlyentries: {
+            $push: { month: "$_id.month", dailyentries: "$dailyentries" },
+          },
+        },
+      },
+    ],
+
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } // TODO handle error
+      // console.log(result);
+      return result;
+    }
+  );
+
+
+
+
+  var data = {};
   //Pipline to accumulate all Sad classified entries
- data.S= await Entry.aggregate(
+  data.S = await Entry.aggregate(
     [
       { $match: { user: ObjectId(req.body.user) } },
       { $match: { category: "S" } },
@@ -93,7 +180,7 @@ module.exports.getAnnualData =async  function (req, res) {
   );
   //Pipline to accumulate all Neutral [N] classified entries
 
-  data.N= await Entry.aggregate(
+  data.N = await Entry.aggregate(
     [
       { $match: { user: ObjectId(req.body.user) } },
       { $match: { category: "N" } },
@@ -137,7 +224,7 @@ module.exports.getAnnualData =async  function (req, res) {
 
   //Pipline to accumulate all Happy [H] classified entries
 
-  data.H= await Entry.aggregate(
+  data.H = await Entry.aggregate(
     [
       { $match: { user: ObjectId(req.body.user) } },
       { $match: { category: "H" } },
@@ -178,8 +265,8 @@ module.exports.getAnnualData =async  function (req, res) {
       return result;
     }
   );
-  complete.user=data;
-  complete.global=global;
+  complete.user = data;
+  complete.global = global;
   return res.status(200).json(complete);
 };
 module.exports.getTotalData = function (req, res) {};
