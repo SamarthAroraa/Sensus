9152c20110d6995d36ea6d4bf363a36d1392d4f8
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-09-04 13:36:25 +0530

    bug fixes, entry api

--- controllers/api/v1/data.js
+++ controllers/api/v1/data.js
@@ -5,7 +5,48 @@ const ObjectId = require("mongodb").ObjectID;
 
 module.exports.getAnnualData =async  function (req, res) {
   var complete={}
-  // var global = {}
+  var global = {}
+  global.S= await Entry.aggregate(
+    [
+      // { $match: { user: ObjectId(req.body.user) } },
+      { $match: { category: "S" } },
+
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+            day: { $dayOfMonth: "$createdAt" },
+          },
+          count: { $sum: 1 },
+        },
+      },
+
+      {
+        $group: {
+          _id: { year: "$_id.year", month: "$_id.month" },
+          dailyentries: { $push: { day: "$_id.day", count: "$count" } },
+        },
+      },
+
+      {
+        $group: {
+          _id: { year: "$_id.year" },
+          monthlyentries: {
+            $push: { month: "$_id.month", dailyentries: "$dailyentries" },
+          },
+        },
+      },
+    ],
+
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } // TODO handle error
+      // console.log(result);
+      return result;
+    }
+  );
   var data = { };
   //Pipline to accumulate all Sad classified entries
  data.S= await Entry.aggregate(
