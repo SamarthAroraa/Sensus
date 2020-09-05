28481962c4604f399e544953cd603a86493e2217
Ishant Dahiya <ishantdahiya@gmail.com>
2020-09-05 08:03:35 +0530

    production settings

--- config/environment.js
+++ config/environment.js
@@ -1,28 +1,47 @@
 const path = require('path')
 const development = {
-  name: "development",
-  secretOrKey: "5D34F7E618B22B4F45EBE2EAE8CDB",
-  session_cookie_key: "asifhajosicrh89w34u9rjixp3purhw3oir",
-  db: {
-    password: "zuarCppdm4JleJpT",
-    username: "samarth",
-  },
-
-  asset_path: "./assets",
-  smtp: {
-    host: "smtp.gmail.com",
-    service: "gmail",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "teamcodeial",
-      pass: "pack6:ICU",
+    name: "development",
+    secretOrKey: "5D34F7E618B22B4F45EBE2EAE8CDB",
+    session_cookie_key: "asifhajosicrh89w34u9rjixp3purhw3oir",
+    db: {
+        password: "zuarCppdm4JleJpT",
+        username: "samarth",
     },
-  },
-  GOOGLE_APPLICATION_CREDENTIALS: path.join(__dirname, "../config/qwiklabs-gcp-02-3a67ec5cb5bc-75e388f83074.json")
+
+    asset_path: "./assets",
+    smtp: {
+        host: "smtp.gmail.com",
+        service: "gmail",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "teamcodeial",
+            pass: "pack6:ICU",
+        },
+    },
+    GOOGLE_APPLICATION_CREDENTIALS: path.join(__dirname, "../config/qwiklabs-gcp-02-3a67ec5cb5bc-75e388f83074.json")
 };
 const production = {
-  name: "production",
+    name: "production",
+    secretOrKey: "5D34F7E618B22B4F45EBE2EAE8CDB",
+    session_cookie_key: "asifhajosicrh89w34u9rjixp3purhw3oir",
+    db: {
+        password: "zuarCppdm4JleJpT",
+        username: "samarth",
+    },
+
+    asset_path: "client/build",
+    smtp: {
+        host: "smtp.gmail.com",
+        service: "gmail",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "teamcodeial",
+            pass: "pack6:ICU",
+        },
+    },
+    GOOGLE_APPLICATION_CREDENTIALS: path.join(__dirname, "../config/qwiklabs-gcp-02-3a67ec5cb5bc-75e388f83074.json")
 };
 
-module.exports = development;
+module.exports = production;
