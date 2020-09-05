03b963080852194db157e45d4f3970343c844165
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-09-05 19:31:04 +0530

    empty text bug fixed

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.use('/entries', require('./entries'));
 router.use('/data', require('./data'));
-router.use('/user-entries', require('./userEntries'));
+// router.use('/user-entries', require('./userEntries'));
 
 module.exports= router;
