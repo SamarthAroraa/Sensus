334299c8952a1bc0fe341a337160f41938f62f3b
Ishant Dahiya <ishantdahiya@gmail.com>
2020-09-05 08:12:55 +0530

    productino setting

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ app.use("/", require("./routes"));
 app.use(express.static(path.join(__dirname, "client/build")))
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, () => {
