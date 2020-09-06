20955c4e87ce28fa1189a60c8cd8e538a2a375f9
Ishant Dahiya <ishantdahiya@gmail.com>
2020-09-06 07:02:47 +0530

    proxies

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -149,7 +149,7 @@ class Dashboard extends React.Component {
       });
 
     //Fetch total data
-    fetch("http://127.0.0.1:5000/api/v1/data/total", {
+    fetch("http://52.206.208.78:5000/api/v1/data/total", {
       method: "post",
       headers: {
         "Content-type": "application/x-www-form-urlencoded",
@@ -169,7 +169,7 @@ class Dashboard extends React.Component {
       });
 
     //Fetch Weekly data
-    fetch("http://127.0.0.1:5000/api/v1/data/weekly", {
+    fetch("http://52.206.208.78:5000/api/v1/data/weekly", {
       method: "post",
 
       headers: {
@@ -208,7 +208,7 @@ class Dashboard extends React.Component {
         });
       });
 
-    fetch("http://127.0.0.1:5000/api/v1/annual")
+    fetch("http://52.206.208.78:5000/api/v1/annual")
   }
 
   render() {
--- client/src/views/EntryList.js
+++ client/src/views/EntryList.js
@@ -37,7 +37,7 @@ const EntryList = (props) => {
   useEffect(() => {
     axios
       .post(
-        "http://127.0.0.1:5000/api/v1/entries/",
+        "http://52.206.208.78:5000/api/v1/entries/",
         qs.stringify({ user: props.auth.user.id })
       )
       // .then((res) => res.json())
@@ -51,7 +51,7 @@ const EntryList = (props) => {
     setEntryList(newEntryList);
     axios
       .post(
-        "http://127.0.0.1:5000/api/v1/entries/delete",
+        "http://52.206.208.78:5000/api/v1/entries/delete",
         qs.stringify({ userId: props.auth.user.id, entryId: entry_id })
       )
       // .then((res) => res.json())
