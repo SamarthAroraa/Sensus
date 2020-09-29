81a383f8e3ea341fa1cccfe5952109ba90840484
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-09-29 22:55:08 +0530

    Connected to front end. Added errors and improved email.

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -8,117 +8,120 @@ import { Link } from "react-router-dom";
 import "../assets/scss/login-form.scss";
 
 import {
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  Button,
-  Card,
-  CardText,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
+	FormGroup,
+	Label,
+	Input,
+	FormText,
+	Button,
+	Card,
+	CardText,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
 } from "reactstrap";
 import {
-  FacebookLoginButton,
-  GoogleLoginButton,
+	FacebookLoginButton,
+	GoogleLoginButton,
 } from "react-social-login-buttons";
 
 const Login = (props) => {
-  const [errors, setErrors] = useState({});
-  const [userCredentials, setUserCredentials] = useState({
-    password: "",
-    email: "",
-  });
-  const danger = {
-    color: "#ff0000",
-  };
-  const { password, email } = userCredentials;
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setUserCredentials({ ...userCredentials, [name]: value });
-  };
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const userData = {
-      email: email,
-      password: password,
-    };
+	const [errors, setErrors] = useState({});
+	const [userCredentials, setUserCredentials] = useState({
+		password: "",
+		email: "",
+	});
+	const danger = {
+		color: "#ff0000",
+	};
+	const { password, email } = userCredentials;
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setUserCredentials({ ...userCredentials, [name]: value });
+	};
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const userData = {
+			email: email,
+			password: password,
+		};
 
-    props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
-  };
+		props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+	};
 
-  useEffect(() => {
-    // If logged in and user navigates to Register page, should redirect them to dashboard
-    if (props.auth.isAuthenticated) {
-      props.history.push("/admin/new-entry");
-    }
-  });
+	useEffect(() => {
+		// If logged in and user navigates to Register page, should redirect them to dashboard
+		if (props.auth.isAuthenticated) {
+			props.history.push("/admin/new-entry");
+		}
+	});
 
-  useEffect(() => {
-    if (props.auth.isAuthenticated) {
-      props.history.push("/admin/new-entry"); // push user to dashboard when they login
-    }
-    if (props.errors) {
-      setErrors(props.errors);
-    }
-  }, [props]);
+	useEffect(() => {
+		if (props.auth.isAuthenticated) {
+			props.history.push("/admin/new-entry"); // push user to dashboard when they login
+		}
+		if (props.errors) {
+			setErrors(props.errors);
+		}
+	}, [props]);
 
-  return (
-    <form className="login-form" noValidate onSubmit={onSubmit}>
-      <Card className="form-component">
-        <CardBody className="p-4">
-          <CardTitle>
-            <h1>Login</h1>
-          </CardTitle>
+	return (
+		<form className="login-form" noValidate onSubmit={onSubmit}>
+			<Card className="form-component">
+				<CardBody className="p-4">
+					<CardTitle>
+						<h1>Login</h1>
+					</CardTitle>
 
-          <FormGroup>
-            <Label for="exampleEmail">Email address</Label>
-            <Input
-              onChange={handleChange}
-              value={email}
-              error={errors.email}
-              type="email"
-              name="email"
-              className={classnames("", {
-                invalid: errors.email || errors.emailnotfound,
-              })}
-              id="exampleEmail"
-              placeholder="Enter email"
-            />
-            <FormText color="danger">
-              {errors.email}
-              {errors.emailnotfound}
-            </FormText>
-            <FormText color="muted">
-              We'll never share your email with anyone else.
-            </FormText>
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">Password</Label>
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-              className={classnames("", {
-                invalid: errors.password || errors.passwordincorrect,
-              })}
-              value={password}
-              onChange={handleChange}
-              error={errors.password}
-              autoComplete="off"
-            />
-            <FormText color="danger">
-              {errors.password}
-              {errors.passwordincorrect}
-            </FormText>
-          </FormGroup>
+					<FormGroup>
+						<Label for="exampleEmail">Email address</Label>
+						<Input
+							onChange={handleChange}
+							value={email}
+							error={errors.email}
+							type="email"
+							name="email"
+							className={classnames("", {
+								invalid: errors.email || errors.emailnotfound,
+							})}
+							id="exampleEmail"
+							placeholder="Enter email"
+						/>
+						<FormText color="danger">
+							{errors.email}
+							{errors.emailnotfound}
+						</FormText>
+						<FormText color="muted">
+							We'll never share your email with anyone else.
+						</FormText>
+					</FormGroup>
+					<FormGroup>
+						<Label for="password">Password</Label>
+						<Input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="Password"
+							className={classnames("", {
+								invalid: errors.password || errors.passwordincorrect,
+							})}
+							value={password}
+							onChange={handleChange}
+							error={errors.password}
+							autoComplete="off"
+						/>
+						<FormText style={{ textAlign: "right" }}>
+							<Link to="/forgot-password">Forgot Password?</Link>
+						</FormText>
+						<FormText color="danger">
+							{errors.password}
+							{errors.passwordincorrect}
+						</FormText>
+					</FormGroup>
 
-          <Button color="primary" className="animation-on-hover" type="submit">
-            Login
-          </Button>
-          {/* <div className="text-center pt-3">
+					<Button color="primary" className="animation-on-hover" type="submit">
+						Login
+					</Button>
+					{/* <div className="text-center pt-3">
             Or log in with your social accounts
           </div>
           <FormGroup className="justify-content-center d-flex pt-2">
@@ -129,22 +132,22 @@ const Login = (props) => {
               <i className="fab fa-facebook-f" />
             </Button>
           </FormGroup> */}
-          <div className="text-center">
-            <Link to="/sign-up">New to Sensus? Sign Up</Link>
-          </div>
-        </CardBody>
-      </Card>
-    </form>
-  );
+					<div className="text-center">
+						<Link to="/sign-up">New to Sensus? Sign Up</Link>
+					</div>
+				</CardBody>
+			</Card>
+		</form>
+	);
 };
 
 Login.propTypes = {
-  loginUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired,
+	loginUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired,
 };
 const mapStateToProps = (state) => ({
-  auth: state.auth,
-  errors: state.errors,
+	auth: state.auth,
+	errors: state.errors,
 });
 export default connect(mapStateToProps, { loginUser })(Login);
--- controllers/forgot_password_controller.js
+++ controllers/forgot_password_controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const mailer = require("../config/nodemailer");
 const bcrypt = require("bcryptjs");
+const forgotPasswordValidation = require("../validation/forgotPassword");
+const { isValidObjectId } = require("mongoose");
 
 const generatePassword = () => {
 	const alphas =
@@ -18,12 +20,14 @@ const generatePassword = () => {
 const mailNewPasswordToUser = async (newPassword, userEmail, callback) => {
 	const subject = "Reset Password";
 
-	const message = `Dear user,
-        We received a request of reset password from your account. We have generated a random temporary password for you which you can later change in the user profile tab.
-        
-    Your new generated Password is: ${newPassword}
-        
-    Regards,
+	const message = `Dear user,<br>
+	&nbsp;&nbsp;&nbsp;&nbsp; We received a request of reset password from your account. We have generated a random temporary password for you which you can later change in the user profile tab.
+    <br><br>    
+	Your new generated Password is: ${newPassword}
+	<br>
+	Get back to your account here: <a href="http://localhost:3000">Sensusjournal.com</a>
+    <br><br>    
+    Regards,<br>
     Team Sensus`;
 
 	await mailer.sendMail(
@@ -31,7 +35,7 @@ const mailNewPasswordToUser = async (newPassword, userEmail, callback) => {
 			from: "sensus.service@gmail.com",
 			to: userEmail,
 			subject: subject,
-			text: message,
+			html: message,
 		},
 		(err) => {
 			if (err) {
@@ -46,43 +50,71 @@ const mailNewPasswordToUser = async (newPassword, userEmail, callback) => {
 module.exports.forgotPassword = async (req, res) => {
 	const email = req.body.email;
 
-	console.log(email);
+	const { isValid, errors } = forgotPasswordValidation({ email: email });
 
-	User.findOne({ email: email }, (err, user) => {
-		if (err) {
-			console.log(err);
-			res.status(500).json({ message: "DB error in finding user", err: err });
-		} else if (!user) {
-			res.status(400).json({ userNotFound: "The Email ID is not registered" });
-		} else {
-			const newPass = generatePassword();
+	if (!isValid) {
+		res
+			.status(400)
+			.json({ message: "Validation failed", displayMessage: errors.email });
+	} else {
+		User.findOne({ email: email }, (err, user) => {
+			if (err) {
+				console.log(err);
+				res.status(500).json({
+					message: "DB error in finding user",
+					err: err,
+					displayMessage: "Something went wrong, please try again later.",
+				});
+			} else if (!user) {
+				res
+					.status(400)
+					.json({ displayMessage: "The Email ID is not registered" });
+			} else {
+				const newPass = generatePassword();
 
-			// Hash password before saving in database
-			bcrypt.genSalt(10, (err, salt) => {
-				bcrypt.hash(newPass, salt, (err, hash) => {
-					if (err) {
-						res
-							.status(500)
-							.json({ message: "Unable to hash password", err: err });
-					}
-					const oldPass = user.password;
-					user.password = hash;
-
-					user.save((err, newUser) => {
+				// Hash password before saving in database
+				bcrypt.genSalt(10, (err, salt) => {
+					bcrypt.hash(newPass, salt, (err, hash) => {
 						if (err) {
-							res.status(500).json({ message: "User not saved", err: err });
-						} else {
-							mailNewPasswordToUser(newPass, email, (err) => {
-								if (err) {
-									res.status(500).json({ message: "Email not sent", err: err });
-								} else {
-									res.status(200).json({ message: "Password Sent to user!" });
-								}
+							res.status(500).json({
+								message: "Unable to hash password",
+								err: err,
+								displayMessage: "Something went wrong, please try again later.",
 							});
 						}
+						const oldPass = user.password;
+						user.password = hash;
+
+						user.save((err) => {
+							if (err) {
+								res.status(500).json({
+									message: "User not saved",
+									err: err,
+									displayMessage:
+										"Something went wrong, please try again later.",
+								});
+							} else {
+								mailNewPasswordToUser(newPass, email, (err) => {
+									if (err) {
+										res.status(500).json({
+											message: "Email not sent",
+											err: err,
+											displayMessage:
+												"Something went wrong, please try again later.",
+										});
+									} else {
+										res.status(200).json({
+											message: "Password Sent to user!",
+											displayMessage:
+												"The password has been sent to your email ID.",
+										});
+									}
+								});
+							}
+						});
 					});
 				});
-			});
-		}
-	});
+			}
+		});
+	}
 };
