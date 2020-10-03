4f6cd22840aa8ba6bcc81e095871924c8f3331aa
pragatikumar <pragatikumar2000@gmail.com>
2020-10-03 16:08:32 +0530

    login- center aligned

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -118,7 +118,7 @@ const Login = (props) => {
 						</FormText>
 					</FormGroup>
 
-					<Button color="primary" className="animation-on-hover" type="submit">
+					<Button color="primary" className="animation-on-hover" type="submit" style={{ marginLeft: "32.8%" }}>
 						Login
 					</Button>
 					{/* <div className="text-center pt-3">
